a274c5cc99ede6214ab0073e79bb4f0e841344ce
savysy <siyangy@seas.upenn.edu>
2017-11-27 01:19:40 -0500

    add in Watson API, configured classpath

---
+++ WatsonAPI/src/main/java/watson/WatsonAPI/App.java
@@ -0,0 +1,13 @@
+package watson.WatsonAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
