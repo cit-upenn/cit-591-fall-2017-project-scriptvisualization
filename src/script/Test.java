505fa26f3d7c52bc3838bcf28d8ad2371d5ea4ef
Yue Yin <yyin@seas.upenn.edu>
2017-12-16 23:08:59 -0500

    script page and main page

--- src/script/Test.java
+++ src/script/Test.java
@@ -1,15 +1,13 @@
 package script;
 
-
-
- 
-
- 
- 
+import java.io.IOException;
 
 public class Test {
 
-	public static void main(String[] args) throws GeneralSecurityException, IOException {
+	public static void main(String[] args) throws IOException{
+		ScriptScraper ss = new ScriptScraper();
+		ss.getMoviesFromGenre(Genre.SCIFI);
+		System.out.println(ss.getMovieList().toString());
  
 		 
 	}
