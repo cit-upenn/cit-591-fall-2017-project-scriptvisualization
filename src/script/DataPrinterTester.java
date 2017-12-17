d5b69ff843f8007f2e54a9574cf7d9b6b1896f07
savysy <siyangy@seas.upenn.edu>
2017-12-16 20:39:35 -0500

    add clas dataPrinter

---
+++ src/script/DataPrinterTester.java
@@ -0,0 +1,23 @@
+package script;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class DataPrinterTester {
+	public static void main(String[] args) throws IOException, GeneralSecurityException {
+		String urlName = "http://www.imsdb.com/scripts/Pearl-Harbor.html";
+		ScriptScraper ss = new ScriptScraper();
+		ScriptReader sr = new ScriptReader();
+		Script script = sr.readScript(ScriptScraper.scrapeScript(urlName), "Pearl Harbor");
+		String content = ss.scrapeScript(urlName);
+		DataPrinter dp = new DataPrinter();
+		
+//		dp.printPersonality(script);
+		
+//		dp.printHeader(content);
+		
+//		dp.printRelation(sr);
+		
+		dp.printKeywords(content);
+	}
+}
